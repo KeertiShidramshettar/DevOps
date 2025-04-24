cff1163f1e772adb1dd197d616a989cd2f12023c
Keerti Shidramshettar <keertishettar2011@gmail.com>
2025-04-24 13:04:48 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ public class Demo{
     public static void main(String[] args) {
         int a= 10;
         int b= 20;
+         System.out.println("Addition: "+(a+b));
+        System.out.println("Subtraction : "+(a-b));
         System.out.println("Multiplication: "+(a*b));
         System.out.println("Division : "+(a/b));
 
