e412769234f83fa007468123982b6d667070575a
Keerti Shidramshettar <keertishettar2011@gmail.com>
2025-04-24 13:05:08 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,8 +4,7 @@ public class Demo{
         int b= 20;
          System.out.println("Addition: "+(a+b));
         System.out.println("Subtraction : "+(a-b));
-        System.out.println("Multiplication: "+(a*b));
-        System.out.println("Division : "+(a/b));
+    
 
     }
 }
